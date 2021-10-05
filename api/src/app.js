50a192f522a213009b8e61723be6bb0022c9a7b2
roshatron2 <roshatron2@gmail.com>
2021-10-05 16:42:59 +0530

    added body parser

--- api/src/app.js
+++ api/src/app.js
@@ -10,6 +10,7 @@ const auth = require("./routes/auth");
 
 connectToDb();
 app.use(express.json());
+app.use(express.urlencoded({ extendded: false }));
 app.use(cors());
 
 const errorHandler = require("./middlewares/errorHandler");
@@ -19,5 +20,5 @@ app.use("/api/v1/auth", auth);
 app.use(errorHandler);
 app.get("/", (req, res) => res.send("Server is UP"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, console.log(`server started in ${process.env.NODE_ENV} mode at port ${PORT}`));
